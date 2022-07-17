1d733e76a0f2f019222c01cefb40a188ff3907fc
m.salehi <msalehi@m.salehi>
2022-07-18 01:03:16 +0430

    [edit] filter date in suggest mobile

--- product/views.py
+++ product/views.py
@@ -1,7 +1,6 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Mobile
 from .serializers import MobileSerializer
-from django.db.models import Q
 
 
 class SuggestMobileView(ListAPIView):
@@ -10,12 +9,17 @@ class SuggestMobileView(ListAPIView):
     def get_queryset(self):
         min_price = self.request.GET.get('min_price', None)
         max_price = self.request.GET.get('max_price', None)
+        min_date = self.request.GET.get('min_date', None)
+        max_date = self.request.GET.get('max_date', None)
         queryset = Mobile.objects.all()
 
         if max_price:
             queryset = queryset.filter(price__lte=max_price)
-
         if min_price:
             queryset = queryset.filter(price__gte=min_price)
+        if max_date:
+            queryset = queryset.filter(production_date__lte=max_date)
+        if min_date:
+            queryset = queryset.filter(production_date__gte=min_date)
 
         return queryset
